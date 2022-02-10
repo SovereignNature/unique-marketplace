4458b20b0a7e01200d7c3e7bffeb469e6493d422
hyorin482 <noraplay00@gmail.com>
2022-02-10 08:53:59 +0100

    Front-end-v11

--- packages/apps/src/MobileMenuHeader.tsx
+++ packages/apps/src/MobileMenuHeader.tsx
@@ -36,19 +36,8 @@ const MobileMenuHeader = (props: Props): React.ReactElement<Props> => {
           className='menu-icon active'
           onClick={setIsMobileMenu.bind(null, 'tokens')}
         >
-          <svg fill='none'
-            height='32'
-            viewBox='0 0 32 32'
-            width='32'
-            xmlns='http://www.w3.org/2000/svg'>
-            <path clipRule='evenodd'
-              d='M4 16C4 15.4477 4.44772 15 5 15H27C27.5523 15 28 15.4477 28 16C28 16.5523 27.5523 17 27 17H5C4.44772 17 4 16.5523 4 16Z'
-              fill='#040B1D'
-              fillRule='evenodd' />
-            <path clipRule='evenodd'
-              d='M14.7071 6.29289C15.0976 6.68342 15.0976 7.31658 14.7071 7.70711L6.41421 16L14.7071 24.2929C15.0976 24.6834 15.0976 25.3166 14.7071 25.7071C14.3166 26.0976 13.6834 26.0976 13.2929 25.7071L4.29289 16.7071C3.90237 16.3166 3.90237 15.6834 4.29289 15.2929L13.2929 6.29289C13.6834 5.90237 14.3166 5.90237 14.7071 6.29289Z'
-              fill='#040B1D'
-              fillRule='evenodd' />
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M3 18H21V16H3V18ZM3 13H21V11H3V13ZM3 6V8H21V6H3Z" fill="#AAE6B9" />
           </svg>
         </div>
       )}
